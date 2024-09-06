385b8c2eeaf6bbc5da957f982fcf02e1802c11b8
NicolasYanB <nicolas07.yan@gmail.com>
2024-09-06 15:03:11 -0300

    add files to session storage

--- front/static/script/index.js
+++ front/static/script/index.js
@@ -1,6 +1,30 @@
 const host = 'http://localhost:8000';
 let audio_file = null;
 
+window.onload = () => {
+  const generated_audio = sessionStorage.getItem('generated_audio');
+  const file = sessionStorage.getItem('file');
+  if (generated_audio != null) {
+    fetch(generated_audio)
+      .then(res => res.blob())
+      .then(blob => {
+        const url = URL.createObjectURL(blob);
+        audio.setAttribute("src", url);
+        audio.style.display = 'block';
+      });
+  }
+
+  if (file != null) {
+    const file_name = sessionStorage.getItem('file_name');
+    fetch(file)
+      .then(res => res.blob())
+      .then(blob => {
+        audio_file = new File([blob], file_name);
+        set_file_name(file_name);
+      })
+  }
+}
+
 function get_url_content() {
   const url_input = document.getElementById('url');
   const content_textarea = document.getElementById('content');
@@ -65,9 +89,15 @@ function get_generated_audio() {
       alert('Oops, there was an error on the server!');
     } else if (response.ok) {
       response.blob().then(binary => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          sessionStorage.setItem('generated_audio', reader.result);
+        }
+        reader.readAsDataURL(binary);
         const audio_url = URL.createObjectURL(binary);
         audio.setAttribute("src", audio_url);
         audio.style.display = 'block';
+        sessionStorage.setItem('generated_audio', audio_url);
       });
     }
     extract_button.disabled = false;
@@ -86,6 +116,7 @@ function handle_file_selection() {
   const file_input = document.getElementById('file');
   const file = file_input.files[file_input.files.length - 1];
   audio_file = file;
+  store_file()
   set_file_name(file.name);
 }
 
@@ -93,7 +124,17 @@ function handle_drop(event) {
   const file = event.dataTransfer.items[0].getAsFile();
   audio_file = file;
   set_file_name(file.name);
+  store_file()
   event.preventDefault();
+}
+
+function store_file() {
+  const reader = new FileReader();
+  reader.onload = () => {
+    sessionStorage.setItem('file', reader.result);
+    sessionStorage.setItem('file_name', audio_file.name);
+  }
+  reader.readAsDataURL(audio_file);
 }
 
 function handle_dragover(event) {
@@ -105,5 +146,7 @@ function remove_file(event) {
   audio_file = null;
   file_name.innerText = "No file chosen";
   file_name.style.textDecoration = 'none';
+  sessionStorage.removeItem('file');
+  sessionStorage.removeItem('file_name');
   event.preventDefault();
 }
