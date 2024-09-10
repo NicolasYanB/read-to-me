ecbb299d89fad71b68348e29422341c7e481bb82
NicolasYanB <nicolas07.yan@gmail.com>
2024-09-10 02:05:43 -0300

    read host header to get host url

--- front/views.py
+++ front/views.py
@@ -3,7 +3,5 @@ import os
 from django.shortcuts import render
 
 def index(request):
-  host = os.environ.get('HOST_URL')
-  if host is None:
-    host = 'http://localhost:8000'
+  host = request.META['HTTP_HOST']
   return render(request, 'index.html', {'host_url': host})
